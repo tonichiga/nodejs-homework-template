15bcecd1b2500c1a0122a2490bcb82d1da913713
Anton Chyhariev <tonichiga@gmail.com>
2021-06-06 17:58:38 +0300

    hw5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const boolParser = require("express-query-boolean");
 const helmet = require("helmet");
 const limiter = require("./helpers/limiter");
@@ -13,12 +14,17 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(helmet());
 
+require("dotenv").config();
+const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
+app.use(express.static(path.join(__dirname, AVATARS_OF_USERS)));
+
 //  apply to all requests
 app.use(limiter);
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(boolParser());
+
 app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { required } = require("joi");
 const router = express.Router();
 const guard = require("../../../helpers/guard");
+const upload = require("../../../helpers/upload");
 
 const ctrl = require("../../../controllers/users");
 
 router.post("/signup", ctrl.reg);
 router.post("/login", ctrl.login);
 router.post("/logout", guard, ctrl.logout);
+router.patch("/avatars", [guard, upload.single("avatar")], ctrl.avatars);
 
 module.exports = router;
